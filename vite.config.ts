57b5eafabbe9aa30b03434bd5dc74e916c887f39
Manali Dutta <76619647+M-pixel-star@users.noreply.github.com>
2026-02-10 21:01:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/manaliduta/' : '/',
+  base: mode === 'production' ? '/personal_webpage/' : '/',
   server: {
     host: "::",
     port: 8080,
